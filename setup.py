913f1652543194ba5284da9ee7152f2ff9d8aa2b
adam <athornton@lsst.org>
2021-03-05 10:52:56 -0700

    Add new variables to support Butler DP0.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
         "asgiref>=3,<4",
         "pyyaml>=5",
         "rubin_jupyter_utils.helpers>=0.31.1,<1.0",
-        "rubin_jupyter_utils.config>=0.32.0,<1.0",
+        "rubin_jupyter_utils.config>=0.33.0,<1.0",
     ],
     entry_points={
         "console_scripts": [
