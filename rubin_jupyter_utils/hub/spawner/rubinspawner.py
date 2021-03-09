80b6bbb005d9c5d414e1a7639137692c7d452c79
adam <athornton@lsst.org>
2021-03-09 13:48:45 -0700

    Call Moneypenny

--- rubin_jupyter_utils/hub/spawner/rubinspawner.py
+++ rubin_jupyter_utils/hub/spawner/rubinspawner.py
@@ -9,6 +9,8 @@ from rubin_jupyter_utils.helpers import (
     sanitize_dict,
     assemble_gids,
     get_supplemental_gids,
+    call_moneypenny,
+    dossier_from_auth_state,
 )
 from .multispawner import MultiNamespacedKubeSpawner
 from .objects import _create_multus_init_container
@@ -450,6 +452,12 @@ class RubinSpawner(MultiNamespacedKubeSpawner):
                     }
                 }
             )
+        # Check to see if Moneypenny is turned on, and if so, delegate
+        #  provisioning to her.
+        if cfg.turn_on_moneypenny:
+            ast = self.cached_auth_state
+            dossier = dossier_from_auth_state(ast, log=self.log)
+            call_moneypenny(dossier, log=self.log)
         self.set_user_namespace()
         daskconfig = None
         if cfg.allow_dask_spawn:
